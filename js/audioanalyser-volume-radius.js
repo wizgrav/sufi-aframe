06309c83847fd26c2bfac88384458ad5e7f1638d
badfrienddigital <badfrienddigital@gmail.com>
2017-01-07 19:34:28 -0500

    Added "speakers" for spatial audio
    These might be dumb.
    They’re at least good placeholders for now.

---
+++ js/audioanalyser-volume-radius.js
@@ -0,0 +1,22 @@
+AFRAME.registerComponent('audioanalyser-volume-radius', {
+  schema: {
+    analyserEl: {type: 'selector'},
+    multiplier: {type: 'number', default: 1}
+  },
+
+  tick: function () {
+    var analyserEl = this.data.analyserEl || this.el;
+    var analyserComponent;
+    var el = this.el;
+    var volume;
+
+    analyserComponent = analyserEl.components.audioanalyser;
+    if (!analyserComponent.analyser) { return; }
+
+    volume = analyserComponent.volume * this.data.multiplier;
+    el.setAttribute('scale', {
+      x: volume + .5,
+      y: 1,
+      z: volume + .5});
+    }
+});
